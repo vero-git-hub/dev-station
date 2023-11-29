44246f71a368df90ec7913ad8546ab988a3af299
v_code <12273445-v_code@users.noreply.gitlab.com>
2023-11-29 12:11:55 +0200

    v0.1.20: scripts - form localization

--- src/main/java/com/dev/station/controller/forms/AddProgramFormController.java
+++ src/main/java/com/dev/station/controller/forms/AddProgramFormController.java
@@ -1,19 +1,38 @@
 package com.dev.station.controller.forms;
 
+import com.dev.station.manager.LanguageManager;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
-public class AddProgramFormController {
-    @FXML
-    private TextField programNameField;
-    @FXML private TextField programPathField;
+import java.util.ResourceBundle;
 
-    @FXML
-    private void handleSave(ActionEvent event) {
+public class AddProgramFormController {
+    @FXML public Label programNameLabel;
+    @FXML public Label pathToExecutableLabel;
+    @FXML public Button saveButton;
+    @FXML public Button cancelButton;
+    @FXML private TextField programNameField;
+    @FXML private TextField programPathField;
+    ResourceBundle bundle;
+
+    @FXML private void initialize() {
+        bundle = LanguageManager.getResourceBundle();
+        programNameLabel.setText(getTranslate("scriptsProgramNameLabel"));
+        pathToExecutableLabel.setText(getTranslate("scriptsPathToExecutableLabel"));
+        saveButton.setText(getTranslate("scriptsSaveButton"));
+        cancelButton.setText(getTranslate("scriptsCancelButton"));
     }
 
-    @FXML
-    private void handleCancel(ActionEvent event) {
+    @FXML private void handleSave(ActionEvent event) {
+    }
+
+    @FXML private void handleCancel(ActionEvent event) {
+    }
+
+    private String getTranslate(String key) {
+        return bundle.getString(key);
     }
 }
--- src/main/java/com/dev/station/controller/sidebar/ScriptsController.java
+++ src/main/java/com/dev/station/controller/sidebar/ScriptsController.java
@@ -1,5 +1,6 @@
 package com.dev.station.controller.sidebar;
 
+import com.dev.station.manager.LanguageManager;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -8,17 +9,23 @@ import javafx.scene.control.Button;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
+import java.util.ResourceBundle;
+
 public class ScriptsController {
     @FXML public Button addProgramButton;
+    ResourceBundle bundle;
 
-    @FXML
-    private void handleAddProgram() {
+    @FXML private void initialize() {
+        bundle = LanguageManager.getResourceBundle();
+    }
+
+    @FXML private void handleAddProgram() {
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/ui/forms/AddProgramForm.fxml"));
             Parent root = loader.load();
 
             Stage stage = new Stage();
-            stage.setTitle("Add Program");
+            stage.setTitle(getTranslate("scriptsAdditionFormTitle"));
             stage.initModality(Modality.APPLICATION_MODAL);
             stage.setScene(new Scene(root));
 
@@ -26,5 +33,9 @@ public class ScriptsController {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private String getTranslate(String key) {
+        return bundle.getString(key);
     }
 }
