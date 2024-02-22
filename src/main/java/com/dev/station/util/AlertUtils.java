8385481e39a65b1d920f99f0328c2f01266e185c
v_code <12273445-v_code@users.noreply.gitlab.com>
2024-02-22 20:06:38 +0200

    v0.2.48: Success alert style - center position

--- src/main/java/com/dev/station/util/AlertUtils.java
+++ src/main/java/com/dev/station/util/AlertUtils.java
@@ -2,29 +2,26 @@ package com.dev.station.util;
 
 import javafx.animation.PauseTransition;
 import javafx.geometry.Pos;
-import javafx.scene.control.*;
-import javafx.scene.control.Alert.AlertType;
-import javafx.application.Application;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
 import javafx.scene.control.DialogPane;
-import javafx.scene.Node;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.StackPane;
-import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import javafx.util.Duration;
 
-import java.util.Optional;
 
 public class AlertUtils {
 
     public static String PATH_TO_STYLE_FILE = "/styles/style.css";
     public static String PATH_TO_IMAGE_SUCCESS_ALERT = "/images/alert/check-mark-32.png";
+    private static final String PATH_TO_CLOSE_ICON = "/images/alert/close-24.png";
 
     public static void showErrorAlert(String title, String content) {
         Alert alert = new Alert(AlertType.ERROR);
@@ -42,7 +39,6 @@ public class AlertUtils {
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
         alert.initStyle(StageStyle.TRANSPARENT);
         alert.setHeaderText(null);
-        alert.setContentText(message);
 
         DialogPane dialogPane = alert.getDialogPane();
         dialogPane.getStylesheets().add(AlertUtils.class.getResource(PATH_TO_STYLE_FILE).toExternalForm());
@@ -54,15 +50,35 @@ public class AlertUtils {
 
         dialogPane.getButtonTypes().clear();
 
+        HBox content = new HBox();
+        content.setSpacing(10);
+        content.setAlignment(Pos.CENTER_LEFT);
+
         ImageView imageView = new ImageView(new Image(AlertUtils.class.getResourceAsStream(PATH_TO_IMAGE_SUCCESS_ALERT)));
-        dialogPane.setGraphic(imageView);
+        imageView.setPreserveRatio(true);
+        imageView.setFitHeight(24);
 
-        ButtonType closeButton = new ButtonType("X");
-        alert.getButtonTypes().add(closeButton);
+        StackPane textContainer = new StackPane(new Label(message));
+        textContainer.setAlignment(Pos.CENTER_LEFT);
 
-        dialogPane.lookupButton(closeButton).addEventFilter(MouseEvent.MOUSE_CLICKED, event -> {
-            stage.close();
-        });
+        Image closeImage = new Image(AlertUtils.class.getResourceAsStream(PATH_TO_CLOSE_ICON));
+        ImageView closeIcon = new ImageView(closeImage);
+        closeIcon.setFitHeight(16);
+        closeIcon.setFitWidth(16);
+        closeIcon.setPreserveRatio(true);
+
+        Button closeButton = new Button();
+        closeButton.setGraphic(closeIcon);
+        closeButton.getStyleClass().add("close-button");
+
+        Region spacer = new Region();
+
+        content.getChildren().addAll(imageView, textContainer, spacer, closeButton);
+
+        dialogPane.setGraphic(null);
+        dialogPane.setContent(content);
+
+        closeButton.setOnAction(event -> stage.close());
 
         alert.show();
 
