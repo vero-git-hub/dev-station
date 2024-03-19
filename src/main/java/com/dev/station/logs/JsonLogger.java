7bc8b7da4e6aa5832ffddae79482b2d076f89d62
v_code <12273445-v_code@users.noreply.gitlab.com>
2024-03-19 19:36:04 +0200

    v0.4.27: Clear logs file

--- src/main/java/com/dev/station/logs/JsonLogger.java
+++ src/main/java/com/dev/station/logs/JsonLogger.java
@@ -1,6 +1,7 @@
 package com.dev.station.logs;
 
 import com.dev.station.model.SettingsModel;
+import com.dev.station.util.alert.AlertUtils;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
@@ -11,6 +12,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ResourceBundle;
 
 public class JsonLogger {
     private static final String LOG_FILE = "ds_logs.json";
@@ -47,6 +49,19 @@ public class JsonLogger {
             }
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    /**
+     * Method for clearing the log file
+     */
+    public static void clearLogs(ResourceBundle bundle) {
+        try {
+            // Write an empty JSON array to clear the file
+            Files.write(Paths.get(LOG_FILE), "[]".getBytes());
+            AlertUtils.showSuccessAlert("", bundle.getString("clearLogs.success"));
+        } catch (IOException e) {
+            AlertUtils.showErrorAlert("", e.getMessage());
         }
     }
 }
